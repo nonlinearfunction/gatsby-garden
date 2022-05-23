ef8e60cf47300bcf709f20fb06ca4aa6f64c8a1c
nonlinearfunction <contact@nonlinearfunction.org>
2022-05-22 23:32:47 -0400

    Add progress bar for slow-loading pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -286,5 +286,14 @@ module.exports = {
         // More options defined here https://purgecss.com/configuration.html#options
       },
     },
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      //options: {
+      // Setting a color is optional.
+      //  color: `tomato`,
+      // Disable the loading spinner.
+      //  showSpinner: false,
+      //},
+    },
   ],
 }
