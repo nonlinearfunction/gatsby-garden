50162969dab872f2ce334d33abb3ad1d543ffbde
nonlinearfunction <contact@nonlinearfunction.org>
2022-08-05 17:19:49 -0400

    Lots of fixes for blog posts.

--- deploy/build_staging.sh
+++ deploy/build_staging.sh
@@ -8,7 +8,9 @@ gatsby new gatsby-garden https://github.com/nonlinearfunction/gatsby-garden
 python3 /home/dave/nonlinearfunction/gatsby-garden/deploy/sanitize.py
 python3 /home/dave/nonlinearfunction/gatsby-garden/deploy/create_dummy_notes.py
 mkdir /home/dave/nonlinearfunction/gatsby-garden/_notes/attachments/
-cp /home/dave/sync/suffering/attachments/* /home/dave/nonlinearfunction/gatsby-garden/_notes/attachments/
+cd /home/dave/sync/suffering/attachments/
+for f in *; do cp "$f" "/home/dave/nonlinearfunction/gatsby-garden/_notes/attachments/${f// /_}"; done
+# cp /home/dave/sync/suffering/attachments/* /home/dave/nonlinearfunction/gatsby-garden/_notes/attachments/
 cd /home/dave/nonlinearfunction/gatsby-garden/
 gatsby clean
 gatsby build
